bf8b2aba5030f4fb618f1de8f28327815dcbf3b8
curtiszimmerman <curtis.zimmerman@gmail.com>
2014-11-26 02:53:55 -0800

    initial commit

---
+++ iirc.js
@@ -0,0 +1,16 @@
+/**
+ * @project node-irc-client
+ * IRC client module
+ * @file irc.js
+ * primary application driver
+ * @author curtis zimmerman
+ * @contact curtis.zimmerman@gmail.com
+ * @license GPLv3
+ * @version 0.0.1a
+ */
+
+module.exports = exports = __irc = (function() {
+	"use strict";
+
+	
+})();
---
+++ test/iirc.test.js
@@ -0,0 +1,11 @@
+/**
+ * @project node-irc-client
+ * IRC client module
+ * @file irc.test.js
+ * primary application driver unit tests
+ * @author curtis zimmerman
+ * @contact curtis.zimmerman@gmail.com
+ * @license GPLv3
+ * @version 0.0.1a
+ */
+
